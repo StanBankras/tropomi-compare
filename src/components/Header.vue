25ca5e281f9d7ea38aa4262b09614f8291041c0d
Stan Bankras <stanbankras@gmail.com>
2021-01-05 10:25:58 +0100

    Add header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,30 @@
 <template>
-  <p>Test <span>Test</span></p>
+  <div class="header">
+    <img src="@/assets/img/earth.svg" alt="earth">
+    <div>
+      <h1>Lock down climate change</h1>
+      <p>How lockdown measures in Europe and Asia changed nitrogen dioxide emissions</p>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-p {
-  font-weight: bold;
-  span {
-    font-size: 100px;
+  .header {
+    background-color: var(--bg-dark);
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    img {
+      margin-right: 4rem;
+      max-width: 200px;
+    }
+    h1 {
+      color: var(--text-contrast);
+    }
+    p {
+      color: var(--text-contrast);
+      max-width: 30rem;
+    }
   }
-}
 </style>
