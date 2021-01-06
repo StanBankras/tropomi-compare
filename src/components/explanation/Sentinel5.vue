dfef4c53748f8b037f2dcb22f7c49000cff6d2b3
sahitj001 <jordy.sahit@live.nl>
2021-01-06 11:17:17 +0100

    finished what Stan started: finished earth globe with satellite HTML/CSS

--- src/components/explanation/Sentinel5.vue
+++ src/components/explanation/Sentinel5.vue
@@ -5,8 +5,12 @@
     </div>
     <div class="satellite">
       <img src="@/assets/img/satellite.svg" alt="satellite">
-      <p class="tag">Sentinel-5 satellite</p>
-      <p>824km altitude</p>
+      <p class="tag">Sentinel-5P satellite</p>
+      <p class="altitude">824km altitude</p>
+    </div>
+    <div class="satellite-info">
+      <p> Sentinel-5 is a satellite launched with a purpose to monitor air pollution in the atmosphere.
+      Using ultraviolet waves, it's able to detect nitrogen dioxide (NO2). </p>
     </div>
   </div>
 </template>
@@ -54,18 +58,44 @@
     .satellite {
       position: absolute;
       right: 10%;
-      top: 0;
+      top: 0%;
       z-index: 100;
       display: flex;
       flex-direction: column;
+      
+      }
       .tag {
-        padding: 0.5rem;
+        margin-top: 1em;
+        padding: 0.5rem 2rem 0.5rem 2rem;
         background-color: var(--text-contrast);
         border-radius: 30px;
-      }
+        align-self: center;
+        font-size: 2em;
       img {
         width: 500px;
       }
+
+    }
+    .altitude{
+      align-self: center;
+      margin-top: 1rem;
+      font-size: 1.5em;
+      color: var(--text-contrast)
+
+    }
+    .satellite-info{
+      position: absolute;
+      z-index: 2;
+      margin-right:10%;
+      background-color: var(--text-box);
+      box-shadow: 0 0 1rem 0.005rem black;
+      padding: 1.75rem 7.5rem 1.75rem 2rem;
+      width: 50.75rem;
+      font-size: 1.75em;
+      border-radius: 40px;
+      color: var(--text-contrast);
+      transform: translate(-28%, 150%);
+      
     }
   }
 </style>
