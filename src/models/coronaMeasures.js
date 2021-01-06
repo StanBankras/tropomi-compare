38a0394c099d408e65e9e2a2778f9530f428ecdd
sahitj001 <jordy.sahit@live.nl>
2021-01-06 15:07:42 +0100

    cleaned json file that contained corona measures from the whole world. the json will only contain the data from the countries we need

---
+++ src/models/coronaMeasures.js
@@ -0,0 +1,44 @@
+const data = require('../assets/countryMeasures.json')
+// const fs = require('fs')
+
+export const coronaMeasures = data.value
+    .filter(getCountries) //filter only the countries we need
+    .map(countryInfo) //map only the values we need of each key
+
+// convertJSON(allData)
+
+// function convertJSON(data) {
+//     const convert = JSON.stringify(data)
+//     fs.writeFile('countries.json', convert, () => {
+//         console.log('saved')
+//     })
+// }
+
+function getCountries(data) {
+    if (
+        data.COUNTRY_TERRITORY_AREA == 'Japan' ||
+        data.COUNTRY_TERRITORY_AREA == 'China' ||
+        data.COUNTRY_TERRITORY_AREA == 'United Kingdom' ||
+        data.COUNTRY_TERRITORY_AREA == 'Italy' ||
+        data.COUNTRY_TERRITORY_AREA == 'India' ||
+        data.COUNTRY_TERRITORY_AREA == 'Iceland' ||
+        data.COUNTRY_TERRITORY_AREA == 'Latvia' ||
+        data.COUNTRY_TERRITORY_AREA == 'South-Korea' ||
+        data.COUNTRY_TERRITORY_AREA == 'Netherlands'
+    ) {
+        return data
+    }
+}
+
+function countryInfo(data) {
+    return {
+        name: data.COUNTRY_TERRITORY_AREA,
+        country_name: data.COUNTRY_CODE,
+        country_code: data.ISO_3166_1_NUMERIC,
+        who_category: data.WHO_CATEGORY,
+        who_subcategory: data.WHO_SUBCATEGORY,
+        who_measure: data.WHO_MEASURE,
+        targeted: data.TARGETED,
+        comments: data.COMMENTS
+    }
+}
