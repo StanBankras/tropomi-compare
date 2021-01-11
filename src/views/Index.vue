668e33a10212e79679c034443a0be0f318922716
Stan Bankras <stanbankras@gmail.com>
2021-01-11 14:00:38 +0100

    Charts works together and added a lot of functionality

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,10 +1,26 @@
 <template>
   <div id="content">
-    <d3/>
+    <div class="wrap">
+      <d3
+        @week="week => selectedWeek = week"
+        @measures="m => measures[0] = m"
+        @measure="m => measure = m"
+        :zoomMeasure="measure"
+        :week="selectedWeek"
+        :width="windowWidth / 2"
+        :id="1"/>
+      <d3
+        @week="week => selectedWeek = week"
+        @measures="m => measures[1] = m"
+        @measure="m => measure = m"
+        :zoomMeasure="measure"
+        :week="selectedWeek"
+        :width="windowWidth / 2"
+        :id="2"/>
+    </div>
     <header-comp/>
     <explanation/>
     <compare/>
-    <chart/>
   </div>
 </template>
 
@@ -12,12 +28,37 @@
 import HeaderComp from '@/components/Header';
 import Explanation from '@/components/explanation/Index';
 import Compare from '@/components/compare/Index';
-import Chart from '@/components/Chart';
 import D3 from '@/components/D3';
 
 export default {
   components: {
-    Compare, HeaderComp, Explanation, Chart, D3
+    Compare, HeaderComp, Explanation, D3
+  },
+  data() {
+    return {
+      selectedWeek: 1,
+      windowWidth: 500,
+      measures: [0, 0],
+      measure: undefined
+    }
+  },
+  mounted() {
+    this.windowWidth = window.innerWidth;
+    window.addEventListener('resize', () => this.onResize());
+  },
+  beforeUnmount() {
+    window.removeEventListener('resize', this.onResize());
+  },
+  methods: {
+    onResize() {
+      this.windowWidth = window.innerWidth;
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.wrap {
+  display: flex;
+}
+</style>
