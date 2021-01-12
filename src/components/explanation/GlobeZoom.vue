2fdc932d010f9e50dfc179b72c3c14d5dcf2d9c6
sahitj001 <jordy.sahit@live.nl>
2021-01-12 10:12:38 +0100

    made the zoomed in globe page

--- src/components/explanation/GlobeZoom.vue
+++ src/components/explanation/GlobeZoom.vue
@@ -1,34 +1,59 @@
 <template>
   <div class="container">
     <div class="world-container">
-			<div class="info-troposphere">
-				<p>Troposhpere</p>
-				<p>0-12km altitude</p>
+      <div class="info-troposphere">
+        <p>Troposhpere</p>
+        <p>0-12km altitude</p>
+      </div>
+      <img class="bigCloud" src="@/assets/img/big-cloud.svg" alt="big cloud" />
+      <img
+        class="smallCloud"
+        src="@/assets/img/small-cloud.svg"
+        alt="small cloud"
+      />
+      <img class="satellite" src="@/assets/img/satellite.svg" alt="satellite" />
+      <img class="arrow" src="@/assets/img/yellow-arrow.svg" alt="arrow" />
+			<div class="textbox1">
+				<h2>Why measure NO2?</h2>
+				<p>
+        NO2 is a gas with a short life span of only a couple of
+        hours, this makes it easy to monitor the source of the pollution. Every
+        time fossil fuels are burned, NO2 will be emitted into the air. This way
+        it's easy to monitor where the most emissions are and what the possible
+        causes can be.
+      </p>
 			</div>
-			<img class="bigCloud" src="@/assets/img/big-cloud.svg" alt="big cloud">
-			<img class="smallCloud" src="@/assets/img/small-cloud.svg" alt="small cloud">
-			<img class="satellite" src="@/assets/img/satellite.svg" alt="satellite">
-			<img class="arrow" src="@/assets/img/yellow-arrow.svg" alt="arrow">
-			<p class="textbox1">Why measure NO2?
-			NO2 is a gas with a short life span of only a couple of hours, this
-			makes it easy to monitor the source of the pollution. Every time
-			fossil fuels are burned, NO2 will be emitted into the air.
-			This way it's easy to monitor where the most emissions are
-			and what the possible causes can be.</p>
-			<p class="textbox2">Where does NO2 reside?
-			NO2 resides in both the troposphere and the stratosphere,
-			we will only be monitoring the troposphere.
-			In the troposphere the changes are mostly the result
-			of human activities.</p>
-			<p class="textbox3">Comparing cities in times of Covid-19.
-			Because of Covid-19, a lot of normal human activities
-			have been suspended. This suspension had a lot of
-			impact on the NO2 emissions. </p>
+
+      <div class="textbox2">
+        <h2>Where does NO2 reside?</h2>
+        <p>
+          NO2 resides in both the troposphere and the stratosphere, we will only
+          be monitoring the troposphere. In the troposphere the changes are
+          mostly the result of human activities.
+        </p>
+      </div>
+
+      <div class="textbox3">
+        <h2>Comparing cities in times of COVID-19</h2>
+        <p>
+          Comparing cities in times of Covid-19. Because of Covid-19, a lot of
+          normal human activities have been suspended. This suspension had a lot
+          of impact on the NO2 emissions.
+        </p>
+      </div>
+
       <div class="circle1"></div>
       <div class="circle2"></div>
       <div class="circle3"></div>
       <div class="circle4"></div>
-      <!-- <img class="circle1" src="@/assets/img/earth-map.svg" alt="map of earth" /> -->
+      <img class="poi1" src="@/assets/img/poi.svg" alt="London" />
+      <img class="poi2" src="@/assets/img/poi.svg" alt="Amsterdam" />
+      <img class="poi3" src="@/assets/img/poi.svg" alt="Milan" />
+      <img class="poi4" src="@/assets/img/poi.svg" alt="Riga" />
+      <img class="poi5" src="@/assets/img/poi.svg" alt="New Delhi" />
+      <img class="poi6" src="@/assets/img/poi.svg" alt="Beijing" />
+      <img class="poi7" src="@/assets/img/poi.svg" alt="Seoul" />
+      <img class="poi8" src="@/assets/img/poi.svg" alt="Tokyo" />
       <img class="globe" src="@/assets/img/earth-map.svg" alt="map of earth" />
     </div>
   </div>
@@ -36,7 +61,7 @@
 
 <style lang="scss" scoped>
 .container {
-  background-image: linear-gradient(#1A1A1A, #1f34a4);
+  background-image: linear-gradient(#1a1a1a, #1f34a4);
   max-width: 100vw;
   min-height: 2500px;
   display: flex;
@@ -98,96 +123,365 @@
     transform: scale(1.5, 1.8);
     bottom: 70em;
   }
-	.satellite{
-		position: absolute;
-		width: 15.625em;
-		height: 9.375em;
-		z-index: 6;
-		top:18em;
-		transform: scaleX(-1);
-	}
-	.textbox1{
-		position: absolute;
-		width: 30em;
-		font-size: 1.875rem;
-		padding: 1.75rem 7.5rem 1.75rem 2rem;
-		box-shadow: 0 0 1rem 0.005rem black;
-		background-color: var(--text-box);
-		border-radius: 40px;
-		color: var(--text-contrast);
-		z-index: 6;
-		margin-left: 2em;
-		transform: translate(0,70%);
-		// top:700px;
-	}
-	.textbox2{
-		position: absolute;
-		width: 30em;
-		font-size: 1.875rem;
-		padding: 1.75rem 7.5rem 1.75rem 2rem;
-		box-shadow: 0 0 1rem 0.005rem black;
-		background-color: var(--text-box);
-		border-radius: 40px;
-		color: var(--text-contrast);
-		z-index: 6;
-		margin-left: 2em;
-		top:38em;
-		right:0.5em;
-	}
-	.textbox3{
-		position: absolute;
-		width: 30em;
-		font-size: 1.875rem;
-		padding: 1.75rem 7.5rem 1.75rem 2rem;
-		box-shadow: 0 0 1rem 0.005rem black;
-		background-color: var(--text-box);
-		border-radius: 40px;
-		color: var(--text-contrast);
-		z-index: 6;
-		margin-left: 2em;
-		top:64.5em;
-		right:9em;
-	}
-	.arrow{
-		position: absolute;
-		height: 20.75em;
-		transform: translate(1300%, 80%);
-		z-index: 6;
-		top:55.25em;
-		left:6.75em;
-	}
-	.bigCloud{
-		position: absolute;
-		top: 90em;
-		right:4em;
-		z-index: 6;
-		transform: rotate(9deg);
-	}
-	.smallCloud{
-		position: absolute;
-		top:95em;
-		transform: rotate(-10deg);
-		left: 3em;
-		z-index: 6;
-	}
-	.info-troposphere{
-		position: absolute;
-		top:81em;
-		left:14em;
+  .satellite {
+    position: absolute;
+    width: 15.625em;
+    height: 9.375em;
+    z-index: 6;
+    top: 18em;
+    transform: scaleX(-1);
+  }
+  .textbox1 {
+    position: absolute;
+    width: 30em;
+    font-size: 1.875rem;
+    padding: 1.75rem 7.5rem 1.75rem 2rem;
+    box-shadow: 0 0 1rem 0.005rem black;
+    background-color: var(--text-box);
+    border-radius: 40px;
+    color: var(--text-contrast);
+    z-index: 6;
+    margin-left: 2em;
+    transform: translate(0, 70%);
+  }
+  .textbox2 {
+    position: absolute;
+    width: 30em;
+    font-size: 1.875rem;
+    padding: 1.75rem 7.5rem 1.75rem 2rem;
+    box-shadow: 0 0 1rem 0.005rem black;
+    background-color: var(--text-box);
+    border-radius: 40px;
+    color: var(--text-contrast);
+    z-index: 6;
+    margin-left: 2em;
+    top: 38em;
+    right: 0.5em;
+  }
+  .textbox3 {
+    position: absolute;
+    width: 30em;
+    font-size: 1.875rem;
+    padding: 1.75rem 7.5rem 1.75rem 2rem;
+    box-shadow: 0 0 1rem 0.005rem black;
+    background-color: var(--text-box);
+    border-radius: 40px;
+    color: var(--text-contrast);
+    z-index: 6;
+    margin-left: 2em;
+    top: 64.5em;
+    right: 9em;
+  }
+  .arrow {
+    position: absolute;
+    height: 19.5em;
+    transform: translate(1600%, 105%);
+    z-index: 6;
+    top: 49.25em;
+    left: 6.75em;
+  }
+  .bigCloud {
+    position: absolute;
+    top: 90em;
+    right: 4em;
+    z-index: 6;
+    transform: rotate(9deg);
+  }
+  .smallCloud {
+    position: absolute;
+    top: 95em;
+    transform: rotate(-10deg);
+    left: 3em;
+    z-index: 6;
+  }
+  .info-troposphere {
+    position: absolute;
+    top: 81em;
+    left: 14em;
 		z-index: 6;
 
-	}
-	.info-troposphere > p:first-child{
-		background-color: var(--text-contrast);
-		padding: 0.2em 1.875em 0.2em 1.875em;
-		border-radius: 25px;
+  }
+  .info-troposphere > p:first-child {
+    background-color: var(--text-contrast);
+    padding: 0.2em 1.875em 0.2em 1.875em;
+    border-radius: 25px;
 		font-size: 2rem;
-
-	}
-	.info-troposphere > p:last-child{
-		color: var(--text-contrast);
-		padding: 0.2em 1.875em 0.2em 1.875em;
-		font-size: 1.725rem;
-	}
+		font-weight: 600;
+  }
+  .info-troposphere > p:last-child {
+    color: var(--text-contrast);
+    padding: 0.2em 1.875em 0.2em 1.875em;
+    font-size: 1.725rem;
+  }
+  .poi1 {
+    //London
+    position: absolute;
+    bottom: 15.5%;
+    left: 11%;
+    z-index: 6;
+  }
+  .poi2 {
+    //Amsterdam
+    position: absolute;
+    bottom: 16%;
+    left: 15.5%;
+    z-index: 6;
+  }
+  .poi3 {
+    //Milan
+    position: absolute;
+    bottom: 11.5%;
+    left: 23.5%;
+    z-index: 6;
+  }
+  .poi4 {
+    //Riga
+    position: absolute;
+    bottom: 18.5%;
+    left: 28%;
+    z-index: 6;
+  }
+  .poi5 {
+    //New Delhi
+    position: absolute;
+    bottom: 5%;
+    left: 38%;
+    z-index: 6;
+  }
+  .poi6 {
+    //Beijing
+    position: absolute;
+    bottom: 5%;
+    right: 22%;
+    z-index: 6;
+  }
+  .poi7 {
+    //Seoul
+    position: absolute;
+    bottom: 8%;
+    right: 18.3%;
+    z-index: 6;
+  }
+  .poi8 {
+    //Tokyo
+    position: absolute;
+    bottom: 8.5%;
+    right: 12%;
+    z-index: 6;
+  }
+  h2 {
+    font-weight: 600;
+    font-size: 1.25em;
+    padding-bottom: 0.25em;
+  }
+  p {
+		font-weight: 400;
+		line-height: 1.2em;
+  }
 }
+@media only screen and (max-width: 320px) {
+	/* mobile 320px wide */
+	.container {
+  background-image: linear-gradient(#1a1a1a, #1f34a4);
+  max-width: 100vw;
+  min-height: 1300px;
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+  align-items: center;
+  position: relative;
+
+  .world-container {
+    margin-top: 10em;
+  }
+  .globe {
+		position: relative;
+		align-content: center;
+    width: 100vw;
+    z-index: 5;
+		bottom: -9em;
+  }
+  .circle1 {
+
+    position: absolute;
+    height: 20vh;
+    width: 100vw;
+    background-color: #6e99d7;
+    border-radius: 50%;
+    z-index: 4;
+
+    transform: scale(2, 2.6);
+    bottom: 1em;
+  }
+  .circle2 {
+    position: absolute;
+    height: 20vh;
+		width: 100vw;
+
+    background-color: #315fd9;
+    border-radius: 50%;
+    z-index: 3;
+    opacity: 0.33;
+    transform: scale(2, 2.6);
+    bottom: 10em;
+  }
+  .circle3 {
+    position: absolute;
+    height: 20vh;
+    width: 100vw;
+    background-color: #243faa;
+    border-radius: 50%;
+    z-index: 2;
+    opacity: 0.15; //0.15
+    transform: scale(2, 2.6);
+    bottom: 20em;
+  }
+  .circle4 {
+    position: absolute;
+    height: 20vh;
+    width: 100vw;
+    background-color: #1f34a4;
+    border-radius: 50%;
+    z-index: 1;
+    opacity: 0.04; //0.04
+    transform: scale( 3, 5 );
+    bottom: 50em;
+  }
+  .satellite {
+    position: absolute;
+    width: 15.625em;
+    height: 7.375em;
+    z-index: 6;
+		top: 5em;
+    transform: scaleX(-1);
+  }
+  .textbox1 {
+    position: relative;
+    width: 95%;
+    font-size: 1rem;
+    padding: 1rem;
+    box-shadow: 0 0 1rem 0.005rem black;
+    background-color: var(--text-box);
+    border-radius: 25px;
+    color: var(--text-contrast);
+    z-index: 6;
+		transform: translate(0, -25%);
+		margin:auto;
+  }
+  .textbox2 {
+    position: relative;
+    width: 95%;
+    font-size: 1rem;
+    padding: 1rem;
+    box-shadow: 0 0 1rem 0.005rem black;
+    background-color: var(--text-box);
+    border-radius: 25px;
+    color: var(--text-contrast);
+		z-index: 6;
+		margin: auto;
+    top: 0;
+		right: 0;
+		transform: translate(0, -15%);
+  }
+  .textbox3 {
+    position: relative;
+    width: 95%;
+    font-size: 1rem;
+    padding: 1rem;
+    box-shadow: 0 0 1rem 0.005rem black;
+    background-color: var(--text-box);
+    border-radius: 25px;
+    color: var(--text-contrast);
+    z-index: 6;
+		top:0;
+		right:0;
+    margin:auto;
+  }
+  .arrow {
+    position: relative;
+    height: 6.2em;
+    transform: translate(1000%, 783%);
+    z-index: 6;
+    top: 0;
+    left: 0;
+  }
+  .bigCloud {
+    position: absolute;
+    top: 90em;
+    right: 4em;
+    z-index: 6;
+    transform: rotate(9deg);
+  }
+  .smallCloud {
+    position: absolute;
+    top: 95em;
+    transform: rotate(-10deg);
+    left: 3em;
+    z-index: 6;
+  }
+  .info-troposphere {
+    position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		transform: translate(68%, 78%);
+		z-index: 6;
+
+  }
+  .info-troposphere > p:first-child {
+    background-color: var(--text-contrast);
+    padding: 0.2em 1.875em 0.2em 1.875em;
+    border-radius: 25px;
+		font-size: 1rem;
+		font-weight: 600;
+		text-align: center;
+  }
+  .info-troposphere > p:last-child {
+    color: var(--text-contrast);
+    padding: 0.2em 1.875em 0.2em 1.875em;
+		font-size: 1rem;
+		text-align: center;
+  }
+  .poi1 {
+    //London
+    visibility: hidden;
+  }
+  .poi2 {
+    //Amsterdam
+    visibility: hidden;
+  }
+  .poi3 {
+    //Milan
+    visibility: hidden;
+  }
+  .poi4 {
+    //Riga
+		visibility: hidden;
+	}
+  .poi5 {
+    //New Delhi
+    visibility: hidden;
+  }
+  .poi6 {
+    //Beijing
+    visibility: hidden;
+  }
+  .poi7 {
+    //Seoul
+    visibility: hidden;
+  }
+  .poi8 {
+    //Tokyo
+    visibility: hidden;
+  }
+  h2 {
+    font-weight: 600;
+    font-size: 1.25em;
+    padding-bottom: 0.25em;
+  }
+  p {
+		font-weight: 400;
+		line-height: 1.2em;
+  }
+}}
 </style>
--- src/components/explanation/Sentinel5.vue
+++ src/components/explanation/Sentinel5.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="satellite">
       <img src="@/assets/img/satellite.svg" alt="satellite">
-      <p class="tag">Sentinel-5P satellite</p>
+      <p class="tag">Sentinel-5 satellite</p>
       <p class="altitude">824km altitude</p>
     </div>
     <div class="satellite-info">
@@ -62,7 +62,7 @@
       z-index: 100;
       display: flex;
       flex-direction: column;
-      
+
       }
       .tag {
         margin-top: 1em;
@@ -70,7 +70,8 @@
         background-color: var(--text-contrast);
         border-radius: 30px;
         align-self: center;
-        font-size: 2em;
+				font-size: 2em;
+				font-weight:600;
       img {
         width: 500px;
       }
@@ -95,7 +96,93 @@
       border-radius: 40px;
       color: var(--text-contrast);
       transform: translate(-28%, 150%);
-      
+
     }
-  }
+	}
+	@media only screen and (max-width: 320px) {
+		  .sentinel {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    .img-wrapper {
+      position: relative;
+      width: 40vw;
+      height: 40vw;
+      background-color: var(--space-blue-opacity);
+      border-radius: 50%;
+      img {
+        width: 250px;
+        z-index: 10;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+      }
+      &::after, &::before {
+        background-color: var(--space-blue-opacity);
+        width: 20vw;
+        height: 20vw;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        border-radius: 50%;
+        content: '';
+        z-index: 2;
+
+      }
+      &::before {
+        width: 30vw;
+        height: 30vw;
+        z-index: 1;
+      }
+    }
+    .satellite {
+      position: absolute;
+      right: 10%;
+      top: 0%;
+      z-index: 100;
+      display: flex;
+			flex-direction: column;
+			width: 10em;
+
+      }
+      .tag {
+        margin-top: 1em;
+        padding: 0.5rem 2rem 0.5rem 2rem;
+        background-color: var(--text-contrast);
+        border-radius: 30px;
+        align-self: center;
+				font-size: 2em;
+				font-weight:600;
+      img {
+        width: 500px;
+      }
+
+    }
+    .altitude{
+      align-self: center;
+      margin-top: 1rem;
+      font-size: 1.5em;
+      color: var(--text-contrast)
+
+    }
+    .satellite-info{
+      position: absolute;
+      z-index: 2;
+      margin-right:10%;
+      background-color: var(--text-box);
+      box-shadow: 0 0 1rem 0.005rem black;
+      padding: 1.75rem 7.5rem 1.75rem 2rem;
+      width: 50.75rem;
+      font-size: 1.75em;
+      border-radius: 40px;
+      color: var(--text-contrast);
+      transform: translate(-28%, 150%);
+
+    }
+	}
+	}
 </style>
