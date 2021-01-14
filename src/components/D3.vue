55801b24ed8c53891c66f6fde6ec770fe09f19f8
Stan Bankras <stanbankras@gmail.com>
2021-01-14 14:54:20 +0100

    Measures added

--- src/components/D3.vue
+++ src/components/D3.vue
@@ -1,95 +1,105 @@
 <template>
-  <div ref="chart" class="chart" :id="`chart-${id}`">
-    <div class="header">
-      <img :src="`https://purecatamphetamine.github.io/country-flag-icons/3x2/${ fullCountry.countryCode }.svg`"/>
-      <h3>{{ fullCountry.city }}</h3>
-    </div>
-    <svg v-if="chartData" :width="chartWidth" :height="chartHeight + 10">
-      <g clip-path="url(#chart)" :transform="`translate(${ yPadding * 2 }, ${ xPadding })`">
-        <rect
-          @click.self="resetZoom"
-          ref="chart"
-          :width="chartWidth - yPadding * 2"
-          :height="chartHeight - xPadding * 2"
-          fill="#F2F2F2"/>
-        <g v-for="(bar, i) in computedBars" :key="bar.title">
+  <div>
+    <div ref="chart" class="chart" :id="`chart-${id}`">
+      <div class="header">
+        <img :src="`https://purecatamphetamine.github.io/country-flag-icons/3x2/${ fullCountry.countryCode }.svg`"/>
+        <h3>{{ fullCountry.city }}</h3>
+      </div>
+      <svg v-if="chartData" :width="chartWidth" :height="chartHeight + 10">
+        <g clip-path="url(#chart)" :transform="`translate(${ yPadding * 2 }, ${ xPadding })`">
           <rect
-            @click="clickBar(bar)"
-            :x="(!barData || !barData[0] || !barData[1] ? 1 : xScale(barData[0] > barData[1] ? barData[1] : barData[0])) + 0.5 * xPadding"
-            :y="chartHeight - xPadding * 2 - barHeight * (i + 1) - i * 5"
-            :height="barHeight"
-            :width="Math.max(barWidth(barData), xScale(15) - xScale(14))"
-            :rx="barHeight / 2"
-            :fill="barColor(bar.title)"
-            style="cursor: pointer;"
-            opacity="0.3"
-            v-for="barData in bar.fromTo" :key="barData"/>
-        </g>
-        <g :transform="`translate(${ xPadding * 0.5 }, ${ 0 })`">
-          <path class="line" :style="`stroke: url(#svgGradient-${id})`" :d="line(slicedChartData)"/>
-        </g>
-      </g>
-      <g transform="translate(0, 40)">
-        <g class="y-axis" fill="none" transform="translate(20, 0)">
-          <g
-            v-for="tick in yTicks"
-            :key="tick"
-            class="tick"
-            opacity="1"
-            font-size="10"
-            font-family="sans-serif"
-            text-anchor="end"
-            :transform="`translate(${ xPadding }, ${ yScale(tick) - yPadding})`"
-          >
-            <line :opacity="tick === 0 ? 1 : 0.1" stroke="currentColor" :x2="chartWidth - xPadding * 2"></line>
-            <text opacity="0.7" fill="currentColor" x="-9" dy="0.32em">{{ tick }}</text>
-          </g>
-        </g>
-        <g class="x-axis" fill="none" :transform="`translate(${ yPadding * 0.5 + 20 }, 0)`">
-          <g
-            v-for="(tick, i) in xTicks"
-            @mouseover="hoverChart(tick)"
-            style="cursor: pointer;"
-            :key="tick"
-            class="tick"
-            opacity="1"
-            font-size="10"
-            font-family="sans-serif"
-            text-anchor="middle"
-            :transform="`translate(${ xScale(tick) + xPadding * 1 }, ${ - xPadding })`"
-          >
-            <line
-              opacity="0.03"
-              :class="{ active: week === tick }"
-              stroke="currentColor"
-              stroke-width="2"
-              x1="0"
-              x2="0"
-              y1="0"
-              :y2="chartHeight - yPadding * 2"/>
-            <g>
-              <rect v-if="week === tick" fill="#F2F2F2" x="-15" :y="chartHeight - 40" width="30px" height="30px"/>
-              <text v-if="chartWidth > 800 || i % 2 === 0 || tick === week" opacity="0.7" fill="currentColor" dy="0.32em" :y="chartHeight - 25">{{ tick }}</text>
-            </g>
-            <g v-if="week === tick" transform="translate(0, 20)">
-              <rect fill="black" x="-25" y="-15" width="50px" height="30px"/>
-              <text color="white" fill="currentColor" dy="0.32em">{{ (chartData.find(x => x[0] === tick) || [0,0])[1].toFixed(2) }}%</text>
-            </g>
-          </g>
-        </g>
-      </g>
-      <defs>
-        <clipPath id="chart">
-          <rect
+            @click.self="resetZoom"
+            ref="chart"
             :width="chartWidth - yPadding * 2"
-            :height="chartHeight - xPadding * 2"/>
-        </clipPath>
-        <linearGradient :id="`svgGradient-${id}`" x1="0%" x2="0%" :y1="`${ maxValue / (maxValue + Math.abs(minValue)) * 100 }%`" y2="100%">
-          <stop class="start" offset="0%" stop-color="#FF6363" stop-opacity="1"/>
-          <stop class="end" offset="0%" stop-color="#6BA1FF" stop-opacity="1"/>
-        </linearGradient>
-      </defs>
-    </svg>
+            :height="chartHeight - xPadding * 2"
+            fill="#F2F2F2"/>
+          <g v-for="(bar, i) in computedBars" :key="bar.title">
+            <rect
+              @click="clickBar(bar)"
+              :x="(!barData.weeks || !barData.weeks[0] || !barData.weeks[1] ? 1 : xScale(barData.weeks[0] > barData.weeks[1] ? barData.weeks[1] : barData.weeks[0])) + 0.5 * xPadding"
+              :y="chartHeight - xPadding * 2 - barHeight * (i + 1) - i * 5"
+              :height="barHeight"
+              :width="Math.max(barWidth(barData.weeks), xScale(15) - xScale(14))"
+              :rx="barHeight / 2"
+              :fill="barColor(bar.title)"
+              style="cursor: pointer;"
+              opacity="0.3"
+              v-for="barData in bar.fromTo" :key="barData"/>
+          </g>
+          <g :transform="`translate(${ xPadding * 0.5 }, ${ 0 })`">
+            <path class="line" :style="`stroke: url(#svgGradient-${id})`" :d="line(slicedChartData)"/>
+          </g>
+        </g>
+        <g transform="translate(0, 40)">
+          <g class="y-axis" fill="none" transform="translate(20, 0)">
+            <g
+              v-for="tick in yTicks"
+              :key="tick"
+              class="tick"
+              opacity="1"
+              font-size="10"
+              font-family="sans-serif"
+              text-anchor="end"
+              :transform="`translate(${ xPadding }, ${ yScale(tick) - yPadding})`"
+            >
+              <line :opacity="tick === 0 ? 1 : 0.1" stroke="currentColor" :x2="chartWidth - xPadding * 2"></line>
+              <text opacity="0.7" fill="currentColor" x="-9" dy="0.32em">{{ tick }}</text>
+            </g>
+          </g>
+          <g class="x-axis" fill="none" :transform="`translate(${ yPadding * 0.5 + 20 }, 0)`">
+            <g
+              v-for="(tick, i) in xTicks"
+              @mouseover="hoverChart(tick)"
+              style="cursor: pointer;"
+              :key="tick"
+              class="tick"
+              opacity="1"
+              font-size="10"
+              font-family="sans-serif"
+              text-anchor="middle"
+              :transform="`translate(${ xScale(tick) + xPadding * 1 }, ${ - xPadding })`"
+            >
+              <line
+                opacity="0.03"
+                :class="{ active: week === tick }"
+                stroke="currentColor"
+                stroke-width="2"
+                x1="0"
+                x2="0"
+                y1="0"
+                :y2="chartHeight - yPadding * 2"/>
+              <g>
+                <rect v-if="week === tick" fill="#F2F2F2" x="-15" :y="chartHeight - 40" width="30px" height="30px"/>
+                <text v-if="chartWidth > 800 || i % 2 === 0 || tick === week" opacity="0.7" fill="currentColor" dy="0.32em" :y="chartHeight - 25">{{ tick }}</text>
+              </g>
+              <g v-if="week === tick" transform="translate(0, 20)">
+                <rect fill="black" x="-25" y="-15" width="50px" height="30px"/>
+                <text color="white" fill="currentColor" dy="0.32em">{{ (chartData.find(x => x[0] === tick) || [0,0])[1].toFixed(2) }}%</text>
+              </g>
+            </g>
+          </g>
+        </g>
+        <defs>
+          <clipPath id="chart">
+            <rect
+              :width="chartWidth - yPadding * 2"
+              :height="chartHeight - xPadding * 2"/>
+          </clipPath>
+          <linearGradient :id="`svgGradient-${id}`" x1="0%" x2="0%" :y1="`${ maxValue / (maxValue + Math.abs(minValue)) * 100 }%`" y2="100%">
+            <stop class="start" offset="0%" stop-color="#FF6363" stop-opacity="1"/>
+            <stop class="end" offset="0%" stop-color="#6BA1FF" stop-opacity="1"/>
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+    <div class="measures">
+      <div
+        class="measure"
+        :style="`color: ${barColor(measure.category)}`"
+        v-for="measure in activeMeasures" :key="measure.measure">
+        {{ measure.measure }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -170,10 +180,22 @@ export default {
         return {
           title: c,
           color: this.measures[0][c].color,
-          fromTo: this.measures[0][c] ? this.calculateBars(this.measures[0][c].measures) : []
+          fromTo: this.measures[0][c] ? this.calculateBars(this.measures[0][c].measures, c) : []
         }
       });
       return mapped;
+    },
+    activeMeasures() {
+      return this.computedBars
+        .filter(b => b.fromTo.find(f => this.week >= f.weeks[0] && this.week <= f.weeks[1]))
+        .map(b => b.fromTo)
+        .flat()
+        .map(b => {
+          return {
+            category: b.category,
+            measure: b.measure
+          }
+        });
     }
   },
   data() {
@@ -199,6 +221,7 @@ export default {
     },
     clickBar(bar) {
       this.$emit('measure', bar.title);
+      console.log(bar);
     },
     resetZoom() {
       this.$emit('measure', undefined);
@@ -206,11 +229,27 @@ export default {
     hoverChart(tick) {
       this.$emit('week', tick)
     },
-    calculateBars(measures) {
+    calculateBars(measures, category) {
       return measures
-        .map(m => [m.startDate === '12-31-2020' ? 53 : this.$date(m.startDate).week(), m.endDate === '12-31-2020' ? 53 : this.$date(m.endDate).week()])
-        .map(d => d[0] > d[1] ? [d[1], d[0]] : d)
-        .map(d => [d[0], d[1] > 52 ? 53 : d[1]]);
+        .map(m => {
+          return {
+            weeks: [m.startDate === '12-31-2020' ? 53 : this.$date(m.startDate).week(), m.endDate === '12-31-2020' ? 53 : this.$date(m.endDate).week()],
+            measure: m.measure
+          }
+        })
+        .map(m => {
+          return {
+            weeks: m.weeks[0] > m.weeks[1] ? [m.weeks[1], m.weeks[0]] : m.weeks,
+            measure: m.measure
+          }
+        })
+        .map(m => {
+          return {
+            weeks: [m.weeks[0], m.weeks[1] > 52 ? 53 : m.weeks[1]],
+            measure: m.measure,
+            category
+          }
+        });
     },
     barWidth(barData) {
       if(!barData || !barData[1] || !barData[0]) return 0;
