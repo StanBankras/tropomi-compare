b8cc548a08e0f99846b5cd6a048d353dcad5e2bb
Stan Bankras <stanbankras@gmail.com>
2021-01-20 11:05:49 +0100

    Add findings

--- src/components/Findings.vue
+++ src/components/Findings.vue
@@ -23,6 +23,47 @@
         </div>
       </div>
     </div>
+    <div class="finding">
+      <h3>Black lives matter protests</h3>
+      <div class="wrap">
+        <div>
+          <img src="@/assets/img/findings/london.png" alt="">
+          <img src="@/assets/img/findings/london_measures.png" alt="">
+          <p>
+            In London, while a lot of measures were still active, were big Black lives matter protests going on in August 2020. This is the only reason
+            this dramatic increase in NO<sub>2</sub> emission can be explained by. The protests must have caused a lot of people to travel towards London, which caused
+            extra emissions.
+          </p>
+        </div>
+        <div>
+          <img src="@/assets/img/findings/riga.png" alt="">
+          <img src="@/assets/img/findings/riga_measures.png" alt="">
+          <p>
+            In Riga there was only a small increase in NO<sub>2</sub> compared to London, although a 50% increase in comparison to previous year is still high.
+            The reason for the increase might have been less COVID infections and more cars on the road again.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="finding">
+      <h3>No measures, but a strong decrease in NO<sub>2</sub></h3>
+      <div class="wrap">
+        <div>
+          <img src="@/assets/img/findings/amsterdam.png" alt="">
+          <p>
+            In Amsterdam, there were barely any measures, but the NO<sub>2</sub> values did go down dramatically. The reason for this is unclear, because we did not
+            find any major events that might have caused these changes.
+          </p>
+        </div>
+        <div>
+          <img src="@/assets/img/findings/riga2.png" alt="">
+          <p>
+            In Riga, same as in Amsterdam, the NO<sub>2</sub> values decreased a lot at the start of the year compared to 2019. Because it's a comparison between
+            two years, the reason cannot simply be winter time, because this happens in every year.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -40,6 +81,7 @@
     font-weight: 600;
   }
   .finding {
+    margin-bottom: 3rem;
     h3 {
       text-align: center;
       margin-bottom: 1rem;
